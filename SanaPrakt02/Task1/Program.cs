885dd2239144d88aa3dbe9d5d3095f71a9361b4c
David Kravchuk <kn223_kdo@student.ztu.edu.ua>
2022-11-12 22:26:56 +0300

    first 3 lines changed

--- SanaPrakt02/Task1/Program.cs
+++ SanaPrakt02/Task1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("hello");
+Console.WriteLine("David");
+Console.WriteLine("and World");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
